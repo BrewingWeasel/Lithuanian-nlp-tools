e3f1fad57a7e24c4ae35113573cfef6348fc1af6
BrewingWeasel <weeaseled@gmail.com>
2022-09-24 15:58:31 -0700

    fixed and hopefully sped up lemmatization

--- lemmatizer.py
+++ lemmatizer.py
@@ -4,21 +4,11 @@ with open('data\\verb_prefixes.txt', 'r', encoding='utf-8') as f:
     PREFIXES = f.read().split('\n') + ['ne', 'be']
 
 with open('data\\allconjs.txt', 'r', encoding='utf-8') as f:
-    CONJS = f.read().split('\n')
+    CONJS = {i.split(', ')[0]: i.split(', ')[1] for i in f.read().split('\n')}
 
 with open('data\\data.txt', 'r', encoding='utf-8') as f:
     VERBS = f.read().split('\n')
 
-def find_shared_root(word, verb):
-    char_num = len(word)
-    try:
-        for i in range(1, len(word)):
-            char_num = i
-            if(word[:-i] in verb.strip('ti')):
-                break
-    except IndexError:
-        return('')
-    return(word[:-char_num])
 
 def nested_vals(d, word):
     for i, val in enumerate(d.values()):
@@ -38,52 +28,37 @@ def nested_vals(d, word):
                 return(cur_key)
 
 
-def main_prefix_removal(verb):
-    if(verb in CONJS):
+def main_prefix_removal(verb, unprefixed_verb):
+    if(verb == unprefixed_verb):
         return(verb)
     for i in PREFIXES:
         if(verb.startswith(i)):
-            if(verb[len(i):] in CONJS):
+            if(verb[len(i):] == unprefixed_verb):
                 return(verb[len(i):])
             else:
-                verb_found = main_prefix_removal(verb[len(i):])
+                verb_found = main_prefix_removal(verb[len(i):], unprefixed_verb)
                 if(verb_found):
                     return(verb_found)
-    return(verb)
 
 
-def get_prefix(word):
-    root = main_prefix_removal(word)
-    return(word.replace(root, ''))
+def get_prefix(word, unlemmatized):
+    root = main_prefix_removal(word, unlemmatized)
+    try:
+        return(word.replace(root, ''))
+    except TypeError:
+        return('')
 
-def get_info(word, verbs):
-    verb_list = [i.split(',')[0] for i in verbs]
-    if(word in verb_list):
-        return(word, 'infinitive')
-    max_val = len(word)
-    prefix = get_prefix(word)
-    word = word.strip(prefix)
-    while(max_val >= 1):
-        for i, verb in enumerate(verb_list):
-            shared_root = find_shared_root(verb, word)
-            if(len(shared_root) >= max_val):
-                conjs = conjugate(prefix + verb)
-                conj_type = nested_vals(conjs, prefix + word)
-                if(conj_type):
-                    return(prefix + verb, conj_type)
-                #Try reflexive version if the normal version didn't work
-                if('s' in word):
-                    conjs = conjugate(prefix + verb + 's')
-                    conj_type = nested_vals(conjs, prefix + word)
-                    if(conj_type):
-                        return(prefix + verb + 's', conj_type)
-        max_val -= 1
-
-
+def get_info(word):
+    for unlemmatized, lemma in CONJS.items():
+        if(word.endswith(unlemmatized)):
+            prefix = get_prefix(word, unlemmatized)
+            if(prefix != ''):
+                word = word.strip(prefix)
+                return [prefix + lemma, conjugate(prefix + lemma)]
 
 
 def get_lemma(word):
-    return get_info(word, VERBS)[0]
+    return get_info(word)[0]
 
 def get_morph(word):
-    return get_info(word, VERBS)[1]
+    return get_info(word)[1]
--- scripts/generate_conjs.py
+++ scripts/generate_conjs.py
@@ -1,19 +1,25 @@
 from conjugator import conjugate
 
-def write_conj(d, f):
+def write_conj(d, i, f):
     for k, v in d.items():
         if(isinstance(v, dict)):
-            write_conj(v, f)
+            write_conj(v, i, f)
         else:
-            f.write(d[k] + '\n')
+            f.write(f'{d[k]}, {i}\n')
 
 
-with open('data.txt', 'r', encoding='utf-8') as f:
+with open('data\\data.txt', 'r', encoding='utf-8') as f:
     verbs = f.read().split('\n')
     verbs = [i.split(',')[0] for i in verbs]
 
 with open('allconjs.txt', 'w', encoding='utf-8') as f:
     for i in verbs:
+        f.write(i + ', ' + i + '\n')
         a = conjugate(i)
-        write_conj(a, f)
+        write_conj(a, i, f)
+    for i in verbs:
+        i = i + 's'
+        f.write(i + ', ' + i + '\n')
+        a = conjugate(i)
+        write_conj(a, i, f)
         
